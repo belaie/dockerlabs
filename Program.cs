0036a2a53e49cb0c2930b18559e0f9eeede5b32b
bElaie <maqsood.bhatti@gmail.com>
2021-02-12 21:10:32 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+         Console.Write("Hello Maqsood");
+
+
+        }
+    }
+}
